55177849fa963eac1a0daf33e7ea35ed67e86e58
Benjamin Borthwick <benjabee@comcast.net>
2022-05-24 17:04:57 -0400

    Changed APIs to account for deprecation of the old one

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # This is a sample Python script.
-from jikanpy import Jikan
+from mal import client
+from mal import enums
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -20,51 +21,45 @@ if __name__ == '__main__':
         while nextLine:
             members.append(file.readline().strip())
             nextLine = file.tell() != eof
-    jikan = Jikan()
+    cli = client.Client('abbf306998a9acabe108f8cf31d7d683')
+    print(cli.get_anime_list("SixGear", limit=1000, status='completed'))
     animelist = {}
 
 
-    for member in members:
-        i = 1
-        while True:
-            print(member + ": Page " + str(i))
-            try:
-                user = jikan.user(member, request="animelist", parameters={'page': i}, argument='completed')
-            except:
-                print(member + " is not a valid user name")
-            else:
-                for anime in user['anime']:
-                    if anime['watching_status'] > 2:
-                        continue
-                    count = animelist.setdefault(anime['title'], [])
-                    animelist[anime['title']].append(member if anime['watching_status']==2 else (member + " (W)"))
-            if (len(user['anime']) < 300):
-                break
-            i += 1
-    animes = sorted(animelist.items(), key=lambda entry: len(entry[1]), reverse=True)
-    with open("results.csv", "w") as results:
-        results.write("Anime")
-        for member in members:
-            results.write("\t" + member)
-        results.write("\n")
-        for anime in animes:
-            try:
-                results.write(anime[0])
-            except:
-                for char in anime[0]:
-                    try:
-                        results.write(char)
-                    except:
-                        results.write(' ')
-            for person in members:
-                if person in anime[1]:
-                    if (person + " (W)") in anime[1]:
-                        results.write("\t2")
-                    else:
-                        results.write("\t1")
-                else:
-                    results.write("\t0")
-            results.write("\n")
+    # for member in members:
+    #     u_list = cli.get_anime_list(member, limit=100, status='completed')
+    #     i = 1
+    #     while u_list is not None:
+    #         for anime in u_list:
+    #             count = animelist.setdefault(anime.entry.title, [])
+    #             animelist[anime.entry.title].append(member)
+    #         print(member + ' Page ' + str(i))
+    #         i += 1
+    #         u_list = u_list.next_page(cli)
+    # animes = sorted(animelist.items(), key=lambda entry: len(entry[1]), reverse=True)
+    # with open("results.csv", "w") as results:
+    #     results.write("Anime")
+    #     for member in members:
+    #         results.write("\t" + member)
+    #     results.write("\n")
+    #     for anime in animes:
+    #         try:
+    #             results.write(anime[0])
+    #         except:
+    #             for char in anime[0]:
+    #                 try:
+    #                     results.write(char)
+    #                 except:
+    #                     results.write(' ')
+    #         for person in members:
+    #             if person in anime[1]:
+    #                 if (person + " (W)") in anime[1]:
+    #                     results.write("\t2")
+    #                 else:
+    #                     results.write("\t1")
+    #             else:
+    #                 results.write("\t0")
+    #         results.write("\n")
 
 
 
