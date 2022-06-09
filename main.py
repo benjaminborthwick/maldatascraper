43e69668a24f511552e9f6dc0b96a10d524b6498
Benjamin <benjabee@comcast.net>
2022-06-09 11:59:16 -0400

    Adding capability to record anime's in Watching, Dropped, On Hold, or Plan to Watch

--- main.py
+++ main.py
@@ -22,44 +22,82 @@ if __name__ == '__main__':
             members.append(file.readline().strip())
             nextLine = file.tell() != eof
     cli = client.Client('abbf306998a9acabe108f8cf31d7d683')
-    print(cli.get_anime_list("SixGear", limit=1000, status='completed'))
     animelist = {}
 
 
-    # for member in members:
-    #     u_list = cli.get_anime_list(member, limit=100, status='completed')
-    #     i = 1
-    #     while u_list is not None:
-    #         for anime in u_list:
-    #             count = animelist.setdefault(anime.entry.title, [])
-    #             animelist[anime.entry.title].append(member)
-    #         print(member + ' Page ' + str(i))
-    #         i += 1
-    #         u_list = u_list.next_page(cli)
-    # animes = sorted(animelist.items(), key=lambda entry: len(entry[1]), reverse=True)
-    # with open("results.csv", "w") as results:
-    #     results.write("Anime")
-    #     for member in members:
-    #         results.write("\t" + member)
-    #     results.write("\n")
-    #     for anime in animes:
-    #         try:
-    #             results.write(anime[0])
-    #         except:
-    #             for char in anime[0]:
-    #                 try:
-    #                     results.write(char)
-    #                 except:
-    #                     results.write(' ')
-    #         for person in members:
-    #             if person in anime[1]:
-    #                 if (person + " (W)") in anime[1]:
-    #                     results.write("\t2")
-    #                 else:
-    #                     results.write("\t1")
-    #             else:
-    #                 results.write("\t0")
-    #         results.write("\n")
+    for member in members:
+        u_list = cli.get_anime_list(member, limit=100, status='completed')
+        i = 1
+        while u_list is not None:
+            for anime in u_list:
+                count = animelist.setdefault(anime.entry.title, [])
+                animelist[anime.entry.title].append(member)
+            print(member + ' Page ' + str(i))
+            i += 1
+            u_list = u_list.next_page(cli)
+        u_list = cli.get_anime_list(member, limit=100, status='watching')
+        while u_list is not None:
+            for anime in u_list:
+                count = animelist.setdefault(anime.entry.title, [])
+                animelist[anime.entry.title].append(member + " /W/")
+            print(member + ' Watching')
+            u_list = u_list.next_page(cli)
+        u_list = cli.get_anime_list(member, limit=100, status='dropped')
+        i = 1
+        while u_list is not None:
+            for anime in u_list:
+                count = animelist.setdefault(anime.entry.title, [])
+                animelist[anime.entry.title].append(member + " /D/")
+            print(member + 'Dropped Page ' + str(i))
+            i += 1
+            u_list = u_list.next_page(cli)
+        u_list = cli.get_anime_list(member, limit=100, status='on hold')
+        i = 1
+        while u_list is not None:
+            for anime in u_list:
+                count = animelist.setdefault(anime.entry.title, [])
+                animelist[anime.entry.title].append(member + " /H/")
+            print(member + 'Hold Page ' + str(i))
+            i += 1
+            u_list = u_list.next_page(cli)
+        u_list = cli.get_anime_list(member, limit=100, status='ptw')
+        i = 1
+        while u_list is not None:
+            for anime in u_list:
+                count = animelist.setdefault(anime.entry.title, [])
+                animelist[anime.entry.title].append(member + " /PTW/")
+            print(member + 'PTW Page ' + str(i))
+            i += 1
+            u_list = u_list.next_page(cli)
+    animes = sorted(animelist.items(), key=lambda entry: len(entry[1]), reverse=True)
+    with open("results.csv", "w") as results:
+        results.write("Anime")
+        for member in members:
+            results.write("\t" + member)
+        results.write("\n")
+        for anime in animes:
+            try:
+                results.write(anime[0])
+            except:
+                for char in anime[0]:
+                    try:
+                        results.write(char)
+                    except:
+                        results.write(' ')
+            for person in members:
+                if person in anime[1]:
+                    results.write("\t1")
+                elif person + " /W/" in anime[1]:
+                    results.write("\tW")
+                elif person + " /D/" in anime[1]:
+                    results.write("\tD")
+                elif person + " /H/" in anime[1]:
+                    results.write("\tH")
+                elif person + " /PTW/" in anime[1]:
+                    results.write("\tPTW")
+                else:
+                    results.write("\t0")
+            results.write("\n")
 
 
 
